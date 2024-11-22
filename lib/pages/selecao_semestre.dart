eb0e5aa668a64adf798539c644f907087055e043
Lucas Silva <97544334+lucas-dos-santos-silva@users.noreply.github.com>
2024-11-22 00:57:03 -0300

    Botoes de logout

--- lib/pages/selecao_semestre.dart
+++ lib/pages/selecao_semestre.dart
@@ -1,129 +1,129 @@
-import 'package:avisa_mais/pages/login.dart';
-import 'package:avisa_mais/pages/pagina_inicial.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
+// import 'package:avisa_mais/pages/login.dart';
+// import 'package:avisa_mais/pages/pagina_inicial.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:flutter/material.dart';
 
-class SemestersSelectionPage extends StatelessWidget {
-  final String courseName;
-  final List<String> semesters;
+// class SemestersSelectionPage extends StatelessWidget {
+//   final String courseName;
+//   final List<String> semesters;
 
-  const SemestersSelectionPage({
-    super.key,
-    required this.courseName,
-    required this.semesters,
-  });
+//   const SemestersSelectionPage({
+//     super.key,
+//     required this.courseName,
+//     required this.semesters,
+//   });
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.logout, color: Colors.black),
-            onPressed: () async {
-              // Deslogar o usuário
-              await FirebaseAuth.instance.signOut();
-              // Navegar para a tela de login
-              Navigator.of(context).pushReplacement(
-                MaterialPageRoute(builder: (context) => const LoginPage()),
-              );
-            },
-          ),
-        ],
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back, color: Colors.black),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
-        elevation: 0,
-        title: Text(
-          courseName,
-          style: const TextStyle(color: Colors.black),
-        ),
-      ),
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.center,
-            colors: [
-              Color.fromARGB(255, 255, 255, 255),
-              Color.fromARGB(255, 87, 106, 50),
-            ],
-          ),
-        ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                'assets/logo_unicv_colorida.png',
-                height: 120,
-                width: 400,
-              ),
-              const SizedBox(height: 24),
-              Container(
-                padding: const EdgeInsets.all(16.0),
-                decoration: BoxDecoration(
-                  color: Colors.white.withOpacity(0.4),
-                  borderRadius: BorderRadius.circular(8.0),
-                  border: Border.all(
-                    color: Colors.white,
-                    width: 1.0,
-                  ),
-                ),
-                child: Column(
-                  children: [
-                    const Text(
-                      'Selecione o semestre:',
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                      ),
-                    ),
-                    const SizedBox(height: 16),
-                    semesters.isEmpty
-                        ? const Text('Nenhum semestre disponível.')
-                        : SizedBox(
-                            height: 350,
-                            width: 320,
-                            child: ListView.builder(
-                              itemCount: semesters.length,
-                              itemBuilder: (context, index) {
-                                return ListTile(
-                                  title: Text(
-                                    semesters[index],
-                                    style: const TextStyle(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.black,
-                                    ),
-                                  ),
-                                  onTap: () {
-                                    Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                        builder: (context) =>
-                                            RecentNotificationsPage(
-                                          selectedSemester: semesters[index],
-                                        ),
-                                      ),
-                                    );
-                                  },
-                                );
-                              },
-                            ),
-                          ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         actions: [
+//           IconButton(
+//             icon: const Icon(Icons.logout, color: Colors.black),
+//             onPressed: () async {
+//               // Deslogar o usuário
+//               await FirebaseAuth.instance.signOut();
+//               // Navegar para a tela de login
+//               Navigator.of(context).pushReplacement(
+//                 MaterialPageRoute(builder: (context) => const LoginPage()),
+//               );
+//             },
+//           ),
+//         ],
+//         leading: IconButton(
+//           icon: const Icon(Icons.arrow_back, color: Colors.black),
+//           onPressed: () {
+//             Navigator.pop(context);
+//           },
+//         ),
+//         backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+//         elevation: 0,
+//         title: Text(
+//           courseName,
+//           style: const TextStyle(color: Colors.black),
+//         ),
+//       ),
+//       body: Container(
+//         decoration: const BoxDecoration(
+//           gradient: LinearGradient(
+//             begin: Alignment.topCenter,
+//             end: Alignment.center,
+//             colors: [
+//               Color.fromARGB(255, 255, 255, 255),
+//               Color.fromARGB(255, 87, 106, 50),
+//             ],
+//           ),
+//         ),
+//         child: Center(
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               Image.asset(
+//                 'assets/logo_unicv_colorida.png',
+//                 height: 120,
+//                 width: 400,
+//               ),
+//               const SizedBox(height: 24),
+//               Container(
+//                 padding: const EdgeInsets.all(16.0),
+//                 decoration: BoxDecoration(
+//                   color: Colors.white.withOpacity(0.4),
+//                   borderRadius: BorderRadius.circular(8.0),
+//                   border: Border.all(
+//                     color: Colors.white,
+//                     width: 1.0,
+//                   ),
+//                 ),
+//                 child: Column(
+//                   children: [
+//                     const Text(
+//                       'Selecione o semestre:',
+//                       style: TextStyle(
+//                         fontSize: 24,
+//                         fontWeight: FontWeight.bold,
+//                         color: Colors.black,
+//                       ),
+//                     ),
+//                     const SizedBox(height: 16),
+//                     semesters.isEmpty
+//                         ? const Text('Nenhum semestre disponível.')
+//                         : SizedBox(
+//                             height: 350,
+//                             width: 320,
+//                             child: ListView.builder(
+//                               itemCount: semesters.length,
+//                               itemBuilder: (context, index) {
+//                                 return ListTile(
+//                                   title: Text(
+//                                     semesters[index],
+//                                     style: const TextStyle(
+//                                       fontSize: 16,
+//                                       fontWeight: FontWeight.bold,
+//                                       color: Colors.black,
+//                                     ),
+//                                   ),
+//                                   onTap: () {
+//                                     Navigator.push(
+//                                       context,
+//                                       MaterialPageRoute(
+//                                         builder: (context) =>
+//                                             RecentNotificationsPage(
+//                                           selectedSemester: semesters[index], selectedCourse: '',
+//                                         ),
+//                                       ),
+//                                     );
+//                                   },
+//                                 );
+//                               },
+//                             ),
+//                           ),
+//                   ],
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
